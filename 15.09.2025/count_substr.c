b9f86ae9dd3a627cd1f2f7f632d22de19f5e84bd
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-16 16:58:29 +0300

    count substring algorithm

---
+++ 15.09.2025/count_substr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int countSubStrs(const char* s, const char* s1)
+{
+    int k = 0;
+    const int len = strlen(s);
+    const int subLen = strlen(s1);
+    
+    for (int i = 0; i < len - subLen + 1; i++) {
+        if (memcmp(s + i, s1, subLen) == 0) 
+            k++;
+    }
+
+    return k;
+}
+
+int main(void)
+{
+    printf("%d\n", countSubStrs("abcabcaaaabccc", "abc"));   
+    printf("%d\n", countSubStrs("aaa", "a"));
+    printf("%d\n", countSubStrs("aaaa", "aa"));
+    return 0;
+}
