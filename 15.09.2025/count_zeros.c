b7d8bc45c0ce573b0da7478bb31eb843a3dc9cff
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-16 17:05:45 +0300

    count zeros algrorithm

---
+++ 15.09.2025/count_zeros.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+int countZeros(const int* arr, int size)
+{
+    if (arr == NULL)
+        return 0;
+
+    int k = 0;
+    for (int i = 0; i < size; i++)
+        k += arr[i] == 0;
+    return k;
+}
+
+int main(void) 
+{
+    int a[] = {1, 2, 0, 4, 5, 0};
+    int b[] = {8, 13, -1, 4, 0};
+    int c[] = {4, 3, 1};
+
+    printf("%d\n", countZeros(a, ARRAY_SIZE(a)));
+    printf("%d\n", countZeros(b, ARRAY_SIZE(b)));
+    printf("%d\n", countZeros(c, ARRAY_SIZE(c)));
+    return 0;
+}
