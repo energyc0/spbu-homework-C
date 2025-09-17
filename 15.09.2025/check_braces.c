9fb6b9d1e883eba132017402b56a7fb45d47a44a
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-17 17:51:30 +0300

    fixed isClosedBraces() for differen brace types

--- 15.09.2025/check_braces.c
+++ 15.09.2025/check_braces.c
@@ -3,17 +3,48 @@
 /*
  * 1    - braces are closed
  * 0    - braces are not closed
- * -1   - undefined character
+ * -1   - undefined character or unknown brace
  */
 
 int isClosedBraces(const char* str)
 {
+    char start;
+    char end;
+    switch (str[0]) {
+    case '(':
+        start = '(';
+        end = ')';
+        break;
+    case '[':
+        start = '[';
+        end = ']';
+        break;
+    case '{':
+        start = '{';
+        end = '}';
+        break;
+    case '<':
+        start = '<';
+        end = '>';
+        break;
+    case ')':
+    case ']':
+    case '}':
+    case '>':
+        return 0;
+    case '\0':
+        return 1;
+    default:
+        return -1;
+    }
+
     int k = 0;
     for (const char* p = str; *p != '\0'; p++) {
-        if (*p == '(') {
+        if (*p == start) {
             k++;
-        } else if (*p == ')') {
-            if (--k < 0)
+        } else if (*p == end) {
+            --k;
+            if (k < 0)
                 return -1;
         }
     }
@@ -22,12 +53,15 @@ int isClosedBraces(const char* str)
 
 void printOutput(const char* str)
 {
-    printf("isClosedBraces(%s) = %d\n", str, isClosedBraces(str));
+    printf("isClosedBraces(\"%s\") = %d\n", str, isClosedBraces(str));
 }
 
 int main()
 {
     printOutput("()()((()()))");
     printOutput("(0()))");
-    printOutput("()(");
+    printOutput("<><><");
+    printOutput("][][");
+    printOutput("[][[[]]]");
+    printOutput("{}{{}}");
 }
