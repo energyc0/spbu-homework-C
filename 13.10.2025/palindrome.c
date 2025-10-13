67efe6122e87fb69fc1dd0c4be59e1a6dfdd27d0
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-10-13 14:25:32 +0300

    Control work tasks.
    Made even fibonacci numbers counter.
    Made gnome sort algorithm.
    Made palindrome checker.

---
+++ 13.10.2025/palindrome.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define TEST1 "abcdcba"
+#define TEST2 "was it a cat i saw"
+#define TEST3 "sit on a potato pan otis"
+#define TEST4 "abc"
+#define TEST5 "abc aba"
+
+/*
+ * Return true if the given string is palindrome.
+ * Skips spaces.
+ */
+bool isPalindrome(const char* s)
+{
+    const char* start = s;
+    const char* end = start + strlen(s) - 1;
+
+    for (; start < end; start++, end--) {
+        for (; start < end && isspace(*start); start++)
+            ;
+        for (; start < end && isspace(*end); end--)
+            ;
+
+        if (*start != *end)
+            return false;
+    }
+
+    return true;
+}
+
+/*
+ * Pretty output of checking whether the string is palindrome.
+ */
+void printIsPalindrome(const char* s)
+{
+    printf("'%s' is %s\n", s, isPalindrome(s) ? "palindrome" : "not palindrome");
+}
+
+int main(void)
+{
+    printIsPalindrome(TEST1);
+    printIsPalindrome(TEST2);
+    printIsPalindrome(TEST3);
+    printIsPalindrome(TEST4);
+    printIsPalindrome(TEST5);
+    return 0;
+}
