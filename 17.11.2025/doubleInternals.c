6f8d22aaf7a27eebd0eb1bca76892655a1522f41
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-11-24 13:57:51 +0300

    Restructured doubleInternals.c Empty testing function Separate function to print usage Separate function to convert to exponential form

--- 17.11.2025/doubleInternals.c
+++ 17.11.2025/doubleInternals.c
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
 
 #define MANTISSA_MASK (((uint64_t)1 << 52) - 1)
 #define EXPONENT_MASK ((((uint64_t)1 << 11) - 1) << 52)
@@ -29,13 +32,17 @@ bool scanNumber(double* num, const char* prompt)
     return res == 1;
 }
 
-int main(int argc, char** argv)
+/*
+ * Prints 'number' in 'buf' in exponential form.
+ * sm*2^p, where
+ * s - sign,
+ * m - mantissa,
+ * p - power.
+ * 'buf' must have enough space to store data.
+ */
+char* doubleToExpForm(double number, char* buf)
 {
-    FloatingNumber num = { .number = 0.0 };
-    if (!scanNumber(&num.number, "Please, enter a number:"))
-        return 1;
-
-    printf("You've entered %lf\n", num.number);
+    FloatingNumber num = { .number = number };
 
     char sign = EXTRACT_SIGN(num.bits);
     uint32_t exponent = EXTRACT_EXPONENT(num.bits);
@@ -45,8 +52,60 @@ int main(int argc, char** argv)
      * Formula to extract floating number is
      * sign * (1 + mantissa / 2^52) * 2^(exponent - 1023)
      */
-    printf("It can be represented as %c%.20g*2^%d\n",
-        sign, (1 + mantissa / ((uint64_t)1 << 52)), exponent - 1023);
+    sprintf(buf, "%c%.20g*2^%d",
+    sign, (1 + mantissa / ((uint64_t)1 << 52)), exponent - 1023);
+
+    return buf;
+}
+
+#ifdef DEBUG
+int launchTests()
+{
+    int returnCode = 0;
+    return returnCode;
+}
+
+void printUsage(FILE* stream, const char* programName)
+{
+    const char* flags[] = {"--help", "--test"};
+    const char* description[] = {"Show help info.", "Launch tests."};
+
+    fprintf(stream, "Usage: %s [flags]\n", programName);
+    fprintf(stream, "FLAGS:\n");
+    for (int i = 0; i < (int)ARRAY_SIZE(flags); i++)
+        fprintf(stream, "  %-15s %s\n", flags[i], description[i]);
+}
+#endif
+
+int main(int argc, char** argv)
+{
+    #ifdef DEBUG
+    if (argc > 2) {
+        printUsage(stderr, argv[0]);
+        return 1;
+    } else if (argc == 2) {
+        if (strcmp(argv[1], "--test") == 0)
+            return launchTests();
+        if (strcmp(argv[1], "--help") == 0) {
+            printUsage(stdout, argv[0]);
+            return 0;
+        }
+        printUsage(stderr, argv[0]);
+        return 1;
+    }
+    #else
+    /* Unused. */
+    ((void)argc);
+    ((void)argv);
+    #endif
+
+    double num = 0.0;
+    if (!scanNumber(&num, "Please, enter a number:"))
+        return 1;
+
+    char buf[128] = {0};
+    printf("You've entered %lf\n", num);
+    printf("It can be represented as %s\n", doubleToExpForm(num, buf));
 
     return 0;
 }
