5ccce4024ed53d9cc15e1a567f6f232f9376712f
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-12-01 15:08:12 +0300

    Added tests

--- 01.12.2025/queueTask.c
+++ 01.12.2025/queueTask.c
@@ -1,19 +1,59 @@
 #include "queue.h"
 #include <stdio.h>
 
-int main()
+/*
+ * Return 1 on fail,
+ * Return 0 on success.
+ */
+int launchTests()
 {
     Queue* q = makeQueue();
-
+    int vals[] = {0,1,2,3,4,5,6,7,8,9,10};
+    
     for (int i = 0; i < 10; i++)
-        enqueue(q, i);
+        if(!enqueue(q, i))
+            return 1;
 
     for (int i = 0; i < 10; i++) {
         int n = 0;
-        dequeue(q, &n);
-        printf("num: %d\n", n);
+        if(!dequeue(q, &n))
+            return 1;
+        if (vals[i] != n)
+            return 1;
     }
 
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+
+    int n = 0;
+    if(!dequeue(q, &n) || n != 10)
+        return 1;
+
+    enqueue(q, 11);
+    enqueue(q, 12);
+    enqueue(q, 13);
+    if (!dequeue(q, &n) || n != 20)
+        return 1;
+    if (!dequeue(q, &n) || n != 30)
+        return 1;
+    if (!dequeue(q, &n) || n != 11)
+        return 1;
+    if (!dequeue(q, &n) || n != 12)
+        return 1;
+    if (!dequeue(q, &n) || n != 13)
+        return 1;
+
+    if (dequeue(q, &n))
+        return 1;
+    
     freeQueue(&q);
-    return 0;
+    return q == NULL;
+}
+
+int main()
+{
+    int returnCode = launchTests();
+    printf("Tests %s\n", returnCode == 0 ? "passed" : "failed");
+    return returnCode;
 }
