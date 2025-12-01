34da2c53a175b0437c7079078a2916d349dea27a
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-12-01 14:03:48 +0300

    Ran clang-format

--- 01.12.2025/binaryCompare.c
+++ 01.12.2025/binaryCompare.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-/* 
+/*
  * Compare two unsigned numbers.
  * Return 0, if a == b,
  * Return 1, if a > b,
@@ -12,11 +12,11 @@
  */
 int binaryCompare(unsigned int a, unsigned int b)
 {
-    const int totalBits = sizeof(a)*8;
+    const int totalBits = sizeof(a) * 8;
     /* Start compate from the biggest byte. */
-    for (int bit = totalBits-1; bit >= 0; bit--) {
-        bool hasBitA = (a & (1<<bit));
-        bool hasBitB = (b & (1<<bit));
+    for (int bit = totalBits - 1; bit >= 0; bit--) {
+        bool hasBitA = (a & (1 << bit));
+        bool hasBitB = (b & (1 << bit));
         /* Found the difference */
         if (hasBitA != hasBitB) {
             return hasBitA ? 1 : -1;
@@ -37,14 +37,14 @@ int launchTests()
         unsigned int b;
         int expected;
     } testcases[] = {
-        {11, 12, -1},
-        {UINT_MAX, UINT_MAX, 0},
-        {0, 0, 0},
-        {0, UINT_MAX, -1},
-        {UINT_MAX, 0, 1},
-        {65, 63, 1},
-        {125, 127, -1},
-        {25,25,0}
+        { 11, 12, -1 },
+        { UINT_MAX, UINT_MAX, 0 },
+        { 0, 0, 0 },
+        { 0, UINT_MAX, -1 },
+        { UINT_MAX, 0, 1 },
+        { 65, 63, 1 },
+        { 125, 127, -1 },
+        { 25, 25, 0 }
     };
     int returnCode = 0;
 
@@ -52,8 +52,8 @@ int launchTests()
         int res = binaryCompare(testcases[i].a, testcases[i].b);
         if (res != testcases[i].expected) {
             fprintf(stderr, "binaryCompare(%u, %u) - failed,\n"
-            "expected %d, found %d.\n",
-            testcases[i].a, testcases[i].b, testcases[i].expected, res);
+                            "expected %d, found %d.\n",
+                testcases[i].a, testcases[i].b, testcases[i].expected, res);
             returnCode = 1;
         }
     }
