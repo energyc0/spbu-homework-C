1947f75ef93af5c86d5926717ee7536639d00b3b
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-12-01 13:57:06 +0300

    Added tests, bug in binaryCompare()

--- 01.12.2025/binaryCompare.c
+++ 01.12.2025/binaryCompare.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
 
-unsigned int binaryCompare(unsigned int a, unsigned int b)
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+/* 
+ * Compare two unsigned numbers.
+ * Return 0, if a == b,
+ * Return 1, if a > b,
+ * Return -1, if a < b.
+ */
+int binaryCompare(unsigned int a, unsigned int b)
 {
     const int totalBits = sizeof(a)*8;
     for (int bit = 0; bit < totalBits; bit++) {
         bool hasBitA = (a & (1<<bit));
         bool hasBitB = (b & (1<<bit));
-        /* Find the difference */
+        /* Found the difference */
         if (hasBitA != hasBitB) {
-            return hasBitA ? a : b;
+            return hasBitA ? 1 : -1;
         }
     }
-    /* Return a or b, because they are equal. */
-    return a;
+    return 0;
+}
+
+
+
+int launchTests()
+{
+    struct {
+        unsigned int a;
+        unsigned int b;
+        int expected;
+    } testcases[] = {
+        {11, 12, -1},
+        {INT_MAX, INT_MAX, 0},
+        {INT_MIN, INT_MIN, 0},
+        {INT_MIN, INT_MAX, -1},
+        {INT_MAX, INT_MIN, 1},
+        {1, 0, 0}
+    };
+    int returnCode = 0;
+
+    for (int i = 0; i < ARRAY_SIZE(testcases); i++) {
+        int res = binaryCompare(testcases[i].a, testcases[i].b);
+        if (res != testcases[i].expected) {
+            fprintf(stderr, "binaryCompare(%u, %u) - failed,\n"
+            "expected %d, found %d.\n",
+            testcases[i].a, testcases[i].b, testcases[i].expected, res);
+            returnCode = 1;
+        }
+    }
+
+    printf("Tests %s\n", returnCode == 0 ? "passed" : "failed");
+    return returnCode;
 }
 
 int main(int argc, char** argv)
 {
-    printf("%u\n", binaryCompare(11, 8));
+    launchTests();
     return 0;
 }
